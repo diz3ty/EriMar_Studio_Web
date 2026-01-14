53057dcc07d276a5a3426f8b0da16d9baa5e93cb
Mario Alberto Diaz Zetina <bmario_24@hotmail.com>
2026-01-13 18:53:10 -0600

    modify format

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/servicio/uñas')
+def servicio_uñas():
+    return render_template('nails_services.html')
+
+@app.route('/servicio/pestañas')
+def servicio_pestañas():
+    return render_template('lashes_services.html')
+
+@app.route('/servicio/capilares')
+def servicio_capilares():
+    return render_template('capilar_services.html')
+
+@app.route('/promociones')
+def promociones():
+    return render_template('promociones.html')
+
 # if __name__ == "__main__":
 #    app.run(debug=True)
