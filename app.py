7963ab6abcb333bf844831c5fb68c2fed0bbc182
Mario Alberto Diaz Zetina <bmario_24@hotmail.com>
2026-02-21 13:00:03 -0600

    add format

--- app.py
+++ app.py
@@ -26,5 +26,9 @@ def service_capilar():
 def promos():
     return render_template('promos.html')
 
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
 #if __name__ == "__main__":
 #    app.run(debug=True)
